f1273f53bc154d53bbfea744feb39079b0849917
= <=>
2017-07-01 13:16:08 +0200

    feat: add timestamps to messages

--- models/message.js
+++ models/message.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 const messageSchema = new mongoose.Schema({
   content: String,
-  author: String
+  author: String,
+  timestamp: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('Message', messageSchema);
